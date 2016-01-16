c645a024b67ef3d84832b11ed6defc975b349acf
Roger PR <rpique@gmail.com>
2016-01-16 12:39:54 -0500

    Update logistic.h
    proposed new interface to add continuous variables

--- EM_dap1/logistic.h
+++ EM_dap1/logistic.h
@@ -1,15 +1,17 @@
 #ifndef LOGISTIC_H_   /* Include guard */
 #define LOGISTIC_H_
 
-void logistic_pred(gsl_vector *beta  // Vector of parameters length = 1 + Sum_k(C_k - 1)
+void logistic_pred(gsl_vector *beta  // Vector of parameters length = 1 + Sum_k(C_k - 1) + Kc
 		  ,gsl_matrix_int *X  //Matrix Nobs x K 
 		  ,gsl_vector_int *nlev // Vector with number categories
+		  ,gsl_matrix *Xc   // continuous covariates  Matrix Nobs x Kc (NULL if not used)
 		  ,gsl_vector *yhat //Vector of prob. predicted by the logistic
 		   );
 		 
-int logistic_fit(gsl_vector *beta  // Vector of parameters length = 1 + Sum_k(C_k - 1)
+int logistic_fit(gsl_vector *beta  // Vector of parameters length = 1 + Sum_k(C_k - 1) + Kc
 		 ,gsl_matrix_int *X  //Matrix Nobs x K 
 		 ,gsl_vector_int *nlev // Vector with number categories
+		 ,gsl_matrix *Xc   // continuous covariates  Matrix Nobs x Kc (NULL if not used)
 		 ,gsl_vector *y //Vector of prob. to predict
 		 ,double lambdaL1 // Regularization L1 0.0 if not used
 		 ,double lambdaL2); // Regularization L2 0.0 if not used
